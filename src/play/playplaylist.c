147c8f68f31fa94b99aadda053ec2f1005e158ad
groovthew <mattheuwwijaya@gmail.com>
2023-11-04 13:02:25 +0700

    playsong

---
+++ src/play/playplaylist.c
@@ -0,0 +1,9 @@
+/* File: playplaylist.c */
+
+// Deskripsi: Play Playlist digunakan untuk memainkan lagu berdasarkan id playlist. 
+// Ketika command ini berhasil dieksekusi, queue akan berisi semua lagu yang ada dalma playlist yang dimainkan 
+// dan isi riwayat lagu sama dengan queue, tetapi dengan urutan yang di-reverse
+
+#include <stdio.h>
+#include "queue.h"
+#include "stack.h"
---
+++ src/play/playsong.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "queue.h"
+// File: play
+// KAMUS LOKAL
