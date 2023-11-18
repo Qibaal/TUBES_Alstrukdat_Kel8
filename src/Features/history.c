8b5fe66d2bba442e50dcc869cb74286aeecf9880
groovthew <mattheuwwijaya@gmail.com>
2023-11-18 16:58:59 +0700

    hellour

--- src/Features/history.c
+++ src/Features/history.c
@@ -8,6 +8,5 @@ void history (Stack historySong, Word Command)
     I.S. sembarang
     F.S. menampilkan seluruh current song yang sudah dimainkan. History mungkun kosong */
 {
-    // KAMUS LOKAL
-    int 
+    
 }
